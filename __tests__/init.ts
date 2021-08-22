a567d307dfd82a49552e6e06a5da3a959fc942c7
Djole <djole.nikolic.priv@gmail.com>
2021-08-22 17:55:36 +0200

    adding eslint and prettier

--- __tests__/init.ts
+++ __tests__/init.ts
@@ -1,14 +1,13 @@
-import * as anchor from "@project-serum/anchor";
+import * as anchor from '@project-serum/anchor'
 
-describe("init", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+describe('init', () => {
+    // Configure the client to use the local cluster.
+    anchor.setProvider(anchor.Provider.env())
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Init;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-    expect(tx).toBeTruthy();
-  });
-});
+    it('Is initialized!', async () => {
+        // Add your test here.
+        const program = anchor.workspace.Init
+        const tx = await program.rpc.initialize()
+        expect(tx).toBeTruthy()
+    })
+})
